eb5d999c2bc66e34ab0c9b736b01352425a2311a
Sumaya Távora <31021285+sumayatavora@users.noreply.github.com>
2021-07-26 19:23:13 -0300

    Atualização do site

--- whatsapp.php
+++ whatsapp.php
@@ -8,14 +8,14 @@ $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
 // check if is a mobile
 if ($iphone || $android || $palmpre || $ipod || $berry == true)
 {
-  header('Location: https://api.whatsapp.com/send?phone=5511985562606&text=Olá%20Preciso%20de%20um%20Orçamento!');
+  header('Location: https://api.whatsapp.com/send?phone=551126383723&text=Olá%20Preciso%20de%20um%20Orçamento!');
   //OR
-  echo "<script>window.location='https://api.whatsapp.com/send?phone=5511985562606&text=Olá%20Preciso%20de%20um%20Orçamento!'</script>";
+  echo "<script>window.location='https://api.whatsapp.com/send?phone=551126383723&text=Olá%20Preciso%20de%20um%20Orçamento!'</script>";
 }
 // all others
 else {
-  header('Location: https://web.whatsapp.com/send?phone=5511985562606&text=Olá%20Preciso%20de%20um%20Orçamento!');
+  header('Location: https://web.whatsapp.com/send?phone=551126383723&text=Olá%20Preciso%20de%20um%20Orçamento!');
   //OR
-  echo "<script>window.location='https://web.whatsapp.com/send?phone=5511985562606&text=Olá%20Preciso%20de%20um%20Orçamento!'</script>";
+  echo "<script>window.location='https://web.whatsapp.com/send?phone=551126383723&text=Olá%20Preciso%20de%20um%20Orçamento!'</script>";
 }
 ?>
